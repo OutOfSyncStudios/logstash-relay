007ef2ee925fb84f0628c26db16e7201c3f1a8bb
Jay Reardon <jayreardon@gmail.com>
2017-11-28 14:09:57 -0700

    Fixed broken configuration LAMBDA scripts

--- scripts/configure.js
+++ scripts/configure.js
@@ -22,24 +22,33 @@ const availableRegions = [
   'sa-east-1'
 ];
 
-promptOpts = {
-  properties: []
+const options = {
+  account: null,
+  lambda: null,
+  region: null,
+  bucket: null
+};
+
+const promptOpts = {
+  properties: {}
 };
 
 function performModify() {
+  console.log('aaaa');
+  console.log(JSON.stringify(options));
   modifyFiles(['./simple-proxy-api.yaml', './package.json', './cloudformation.yaml'],
     [{
       regexp: /YOUR_ACCOUNT_ID/g,
-      replacement: program.account
+      replacement: options.account
     }, {
       regexp: /YOUR_AWS_REGION/g,
-      replacement: program.region
+      replacement: options.region
     }, {
       regexp: /YOUR_UNIQUE_BUCKET_NAME/g,
-      replacement: program.bucket
+      replacement: options.bucket
     }, {
       regexp: /YOUR_SERVERLESS_EXPRESS_LAMBDA_FUNCTION_NAME/g,
-      replacement: program.lambda
+      replacement: options.lambda
     }]
   );
 }
@@ -54,17 +63,19 @@ program
   .option('-r, --region <awsRegion>', 'The AWS region to use. Defaults to "us-east-1"');
 
 if (!program.lambda) {
-  promptOpts.properties.push({
+  promptOpts.properties.lambda = {
     name: 'lambda',
     description: 'Enter the AWS Lambda function name',
     default: 'AwsServerlessExpressFunction',
     type: 'string',
     required: true
-  });
+  };
+} else {
+  options.lambda = program.lambda;
 }
 
 if (!program.region || !availableRegions.includes(program.region)) {
-  promptOpts.properties.push({
+  promptOpts.properties.region = {
     name: 'region',
     description: 'Enter an AWS region',
     message: `Must be a valid AWS Region (${availableRegions.join(', ')})`,
@@ -74,38 +85,46 @@ if (!program.region || !availableRegions.includes(program.region)) {
     conform: (v) => {
       return availableRegions.includes(v);
     }
-  });
+  };
+} else {
+  options.region = program.region;
 }
 
 if (!program.account || program.account.length !== 12) {
-  promptOpts.properties.push({
+  promptOpts.properties.account = {
     name: 'account',
     description: 'Supply a 12 digit AWS account ID',
     message: 'Must be a valid 12 digit AWS account',
     type: 'string',
     required: true,
     pattern: /^\w{12}$/
-  });
+  };
+} else {
+  options.account = program.account;
 }
 
 if (!program.bucket) {
-  promptOpts.properties.push({
+  promptOpts.properties.bucket = {
     name: 'bucket',
     description: 'Supply a unique AWS S3 Bucket name',
     message: 'AWS S3 Bucket name can not be blank',
     type: 'string',
     required: true
-  });
+  };
+} else {
+  options.bucket = program.bucket;
 }
 
-if (promptOpts.properties.length !== 0) {
+if (promptOpts.properties !== {}) {
   prompt.start();
   prompt.get(promptOpts, (err, result) => {
     if (result) {
-      if (result.lambda) { program.lambda = result.lambda; }
-      if (result.region) { program.region = result.region; }
-      if (result.account) { program.account = result.account; }
-      if (result.bucket) { program.bucket = result.bucket; }
+      console.log(JSON.stringify(result));
+      if (result.lambda) { options.lambda = result.lambda; }
+      if (result.region) { options.region = result.region; }
+      if (result.account) { options.account = result.account; }
+      if (result.bucket) { options.bucket = result.bucket; }
+      console.log(JSON.stringify(options));
       performModify();
     }
     prompt.stop();
